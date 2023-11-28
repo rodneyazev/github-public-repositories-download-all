6004cb2ad570347becacb615ee3f4e8d959b6f71
rodneyazev <rodney.n.azevedo@gmail.com>
2023-11-28 20:53:43 -0300

    A shell script to download all GitHub public repositories

--- github-public-repositories-download-all.sh
+++ github-public-repositories-download-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
 gh auth login;
-gh repo list rodneyazev --limit 4000 | while read -r repo _; do
+gh repo list <profile> --limit 4000 | while read -r repo _; do
   gh repo clone "$repo" "$repo"
 done
